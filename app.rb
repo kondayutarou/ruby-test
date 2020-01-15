3d95b06e15cf049945d7fe8ce864d9266e8123ba
kondayutarou <kondayutarou@gmail.com>
2020-01-15 17:44:30 +0900

    without exception

--- app.rb
+++ app.rb
@@ -1,16 +1,3 @@
-ltotal = 0
-wtotal = 0
-ctotal = 0
-
-ARGV.each do |file|
-  begin
-    input = File.open(file)
-    i = 0
-    w = 0
-    c = 0
-    input.each_line do |line|
-      l += l
-      c += line.size
-      line.sub!(/^\s+/, "")
-    end
-end
+val = "a"
+n = Integer(val)
+p n
